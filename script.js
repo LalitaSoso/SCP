93eb731da5ff7800908fae21f602f8ee3b611ced
LalitaSoso <127132789+LalitaSoso@users.noreply.github.com>
2023-08-07 15:32:29 -0700

    edited formatting

--- script.js
+++ script.js
@@ -1,59 +1,47 @@
 function displayTime(now) {
-  let hour = now.getHours();
-  if (hour < 10) {
-    hour = `0${hour}`;
-  }
-  let minute = now.getMinutes();
-  if (minute < 10) {
-    minute = `0${minute}`;
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[now.getDay()];
+	let hour = now.getHours();
+	if (hour < 10) {
+		hour = `0${hour}`;
+	}
+	let minute = now.getMinutes();
+	if (minute < 10) {
+		minute = `0${minute}`;
+	}
+	let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	let day = days[now.getDay()];
 
-  return `It is currently ${hour}:${minute} on a ${day} afternoon,`;
+	return `It is currently ${hour}:${minute} on a ${day} afternoon,`;
 }
 
 // week 5 submission ⬇️
 
 function displayCityTempAndDescription(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#just-the-temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector(
-    "#weather-description"
-  ).innerHTML = `with ${response.data.weather[0].description}`;
+	document.querySelector("#city").innerHTML = response.data.name;
+	document.querySelector("#just-the-temp").innerHTML = Math.round(response.data.main.temp);
+	document.querySelector("#weather-description").innerHTML = `with ${response.data.weather[0].description}.`;
 }
 
 function search(city) {
-  let apiKey = "d1a86552de255334f6117b348c4519bd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(displayCityTempAndDescription);
+	let apiKey = "d1a86552de255334f6117b348c4519bd";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+	axios.get(apiUrl).then(displayCityTempAndDescription);
 }
 
 function searchInput(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-input").value;
-  search(city);
+	event.preventDefault();
+	let city = document.querySelector("#search-input").value;
+	search(city);
 }
 
 function searchGeolocation(position) {
-  let apiKey = "d1a86552de255334f6117b348c4519bd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(displayCityTempAndDescription);
+	let apiKey = "d1a86552de255334f6117b348c4519bd";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+	axios.get(apiUrl).then(displayCityTempAndDescription);
 }
 
 function geolocate(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchGeolocation);
+	event.preventDefault();
+	navigator.geolocation.getCurrentPosition(searchGeolocation);
 }
 
 // week 5 submission ⬆️
